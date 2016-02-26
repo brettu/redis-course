bcbf78d6ae63fcd010f65ee513d8d7c5e2df5203
brettu <bweikulrich@me.com>
2016-02-25 23:02:13 -0800

    scaffold update

--- db/migrate/20160226065029_scaffold_application.rb
+++ db/migrate/20160226065029_scaffold_application.rb
@@ -1,6 +1,5 @@
 class ScaffoldApplication < ActiveRecord::Migration
   def change
-
     create_table :users do |t|
       t.string :name
       t.string :email
@@ -9,34 +8,29 @@ class ScaffoldApplication < ActiveRecord::Migration
 
     create_table :animals do |t|
       t.string :name
-      t.integer :pet_value
+      t.integer :value
       t.timestamps null: false
     end
 
     create_table :contests do |t|
       t.string :name
-      t.integer :week
+      t.integer :day
       t.string :prize
-      t.string :vote_type
-      t.integer :choice_one_id
-      t.integer :choice_two_id
-      t.integer :winner_id
       t.timestamps null: false
     end
 
     create_table :clicks do |t|
-      t.integer :animal_id
-      t.integer :user_id
+      t.belongs_to :animal, index: true
+      t.belongs_to :user, index: true
+      t.belongs_to :score_card, index: true
       t.timestamps null: false
     end
 
     create_table :score_cards do |t|
-      t.integer :user_id
-      t.integer :click_id
-      t.integer :contest_id
-      t.integer :value
+      t.belongs_to :user, index: true
+      t.belongs_to :contest, index: true
+      t.integer :score
       t.timestamps null: false
     end
-
   end
 end
